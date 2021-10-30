1a02b4d93036578eec235d557b0ed4b2ac59a70b
willm30 <64402732+willm30@users.noreply.github.com>
2021-10-30 15:45:51 +0200

    custom error

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
 	  res.send('Hello World!')
 })
 
+app.get('/418', (req, res) => {
+  res.set('X-full-stack', '4life');
+  res.status(418);
+  res.send('I prefer coffee');
+})
+
 app.listen(port, () => {
 	  console.log(`Example app listening at http://localhost:${port}`)
 })
